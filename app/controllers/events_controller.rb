62f5b0bc1830a5ede6e5bb22aaa3b40cb2c1c9bd
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-03-01 15:59:43 +0000

    creating events feature done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,9 +12,24 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
+    @group = Group.find(params[:group_id])
   end
 
   def create
+    @event = Event.new(set_event)
+    @group = Group.find(params[:group_id])
+    @event.group = @group
+    if @event.save
+      redirect_to "/groups/#{@event.id}/events/#{@group.id}"
+    else
+      render :new
+    end
   end
 
+  private
+
+  def set_event
+    params.require(:event).permit(:name, :location, :description, :start_date, :end_date)
+  end
 end
