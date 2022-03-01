7703c4494f7d98bfd111465325e53a0ec005dae8
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-03-01 15:15:00 +0000

    invite to group method done

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups do
     resources :events, only: %i[index show new create]
-    resources :usergroups, only: %i[index new create destroy] do
+    resources :usergroups, only: %i[new create destroy] do
       member do
         put :accept
       end
     end
   end
+
+  resources :usergroups, only: %i[index]
 end
