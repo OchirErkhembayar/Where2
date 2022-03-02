b8ed83b73efce5924f40bf2f160f0cbc8bce97e1
Deniz Kazdal <denizkazdal99@gmail.com>
2022-03-02 15:24:13 +0000

    Chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups do
-<<<<<<< HEAD
-    resources :events, only: %i[index show new create] do
-      resources :messages, only: %i[create]
-=======
     resources :events, only: %i[show new create] do
->>>>>>> fa66f1b8f1e5376a9f4227f64426f6748dcf354e
+      resources :messages, only: %i[create]
       resources :event_users, only: %i[create new]
     end
     resources :usergroups, only: %i[new create destroy] do
