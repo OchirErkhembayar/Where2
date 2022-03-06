ce5bd75d5312aa2b1d2a9b5231a650918e7d65bc
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-03-06 23:15:09 +0000

    can't add people already in the group to the group

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,14 @@ class User < ApplicationRecord
     @friends = @friends.reject { |f| @eventusers.include? f }
     @friends.reject { |f| @users.include? f }
   end
+
+  def user_group_invites(group)
+    friendships = Friendship.where('friend_one_id = ? OR friend_two_id = ?', self.id, self.id)
+    first_one = friendships.select { |fs| fs.confirmed == true && (fs.friend_one != self && fs.friend_two == self)}
+    second_one = friendships.select { |fs| fs.confirmed == true && (fs.friend_one == self && fs.friend_two != self)}
+    @friends = first_one.map { |fs| User.find(fs.friend_one.id)}
+    second_one.each { |fs| @friends << User.find(fs.friend_two.id) }
+    @users = UserGroup.where('group_id = ?', group.id).map { |ug| ug.user }
+    @friends.reject { |f| @users.include? f }
+  end
 end
