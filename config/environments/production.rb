66bbac0671ad5924d7b56790c1b9ae1ee6c7df97
Deniz Kazdal <denizkazdal99@gmail.com>
2022-03-09 15:33:20 +0000

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'https://where2-806.herokuapp.com/cable'
+  # config.action_cable.mount_path = '/cable'
+  # config.action_cable.url = 'https://where2-806.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
